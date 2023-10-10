47750ec85707cbb71189e1b3a84b4bcb5fb426eb
robypurbasakty26 <obykao26@gmail.com>
2023-10-10 23:11:22 +0700

    gin

--- api/index.go
+++ api/index.go
@@ -1,46 +1,55 @@
-
-package handler
+package main
 
 import (
-	"fmt"
 	"net/http"
 
-	. "github.com/tbxark/g4vercel"
+	"github.com/gin-gonic/gin"
+	// . "github.com/tbxark/g4vercel"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
+// func Handlmaer(w http.ResponseWriter, r *http.Request) {
+// 	server := New()
 
-	server.GET("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "hello go from vercel !!!!",
+// 	server.GET("/", func(context *Context) {
+// 		context.JSON(200, H{
+// 			"message": "hello go from vercel !!!!",
+// 		})
+// 	})
+// 	server.GET("/hello", func(context *Context) {
+// 		name := context.Query("name")
+// 		if name == "" {
+// 			context.JSON(400, H{
+// 				"message": "name not found",
+// 			})
+// 		} else {
+// 			context.JSON(200, H{
+// 				"data": fmt.Sprintf("Hello %s!", name),
+// 			})
+// 		}
+// 	})
+// 	server.GET("/user/:id", func(context *Context) {
+// 		context.JSON(400, H{
+// 			"data": H{
+// 				"id": context.Param("id"),
+// 			},
+// 		})
+// 	})
+// 	server.GET("/long/long/long/path/*test", func(context *Context) {
+// 		context.JSON(200, H{
+// 			"data": H{
+// 				"url": context.Path,
+// 			},
+// 		})
+// 	})
+// 	server.Handle(w, r)
+// }
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
 		})
 	})
-	server.GET("/hello", func(context *Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, H{
-				"message": "name not found",
-			})
-		} else {
-			context.JSON(200, H{
-				"data": fmt.Sprintf("Hello %s!", name),
-			})
-		}
-	})
-	server.GET("/user/:id", func(context *Context) {
-		context.JSON(400, H{
-			"data": H{
-				"id": context.Param("id"),
-			},
-		})
-	})
-	server.GET("/long/long/long/path/*test", func(context *Context) {
-		context.JSON(200, H{
-			"data": H{
-				"url": context.Path,
-			},
-		})
-	})
-	server.Handle(w, r)
+	r.Run()
 }
