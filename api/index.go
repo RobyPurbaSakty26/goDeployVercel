75cf5999e013eacb7ee5a957d76edcf2441b02eb
robypurbasakty26 <obykao26@gmail.com>
2023-10-11 00:13:57 +0700

    refactor

--- api/index.go
+++ api/index.go
@@ -10,41 +10,45 @@ import (
 // func Handlmaer(w http.ResponseWriter, r *http.Request) {
 // 	server := New()
 
-// 	server.GET("/", func(context *Context) {
-// 		context.JSON(200, H{
-// 			"message": "hello go from vercel !!!!",
-// 		})
-// 	})
-// 	server.GET("/hello", func(context *Context) {
-// 		name := context.Query("name")
-// 		if name == "" {
-// 			context.JSON(400, H{
-// 				"message": "name not found",
-// 			})
-// 		} else {
-// 			context.JSON(200, H{
-// 				"data": fmt.Sprintf("Hello %s!", name),
-// 			})
-// 		}
-// 	})
-// 	server.GET("/user/:id", func(context *Context) {
-// 		context.JSON(400, H{
-// 			"data": H{
-// 				"id": context.Param("id"),
-// 			},
-// 		})
-// 	})
-// 	server.GET("/long/long/long/path/*test", func(context *Context) {
-// 		context.JSON(200, H{
-// 			"data": H{
-// 				"url": context.Path,
-// 			},
-// 		})
-// 	})
-// 	server.Handle(w, r)
-// }
+//		server.GET("/", func(context *Context) {
+//			context.JSON(200, H{
+//				"message": "hello go from vercel !!!!",
+//			})
+//		})
+//		server.GET("/hello", func(context *Context) {
+//			name := context.Query("name")
+//			if name == "" {
+//				context.JSON(400, H{
+//					"message": "name not found",
+//				})
+//			} else {
+//				context.JSON(200, H{
+//					"data": fmt.Sprintf("Hello %s!", name),
+//				})
+//			}
+//		})
+//		server.GET("/user/:id", func(context *Context) {
+//			context.JSON(400, H{
+//				"data": H{
+//					"id": context.Param("id"),
+//				},
+//			})
+//		})
+//		server.GET("/long/long/long/path/*test", func(context *Context) {
+//			context.JSON(200, H{
+//				"data": H{
+//					"url": context.Path,
+//				},
+//			})
+//		})
+//		server.Handle(w, r)
+//	}
+type we struct {
+	w http.ResponseWriter
+}
 
-func Handler(w http.ResponseWriter, req *http.Request) {
+func Handler(req *http.Request) {
+	wriret := we{}
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
@@ -59,6 +63,6 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 		})
 	})
 
-	r.ServeHTTP(w, req)
+	r.ServeHTTP(wriret.w, req)
 
 }
